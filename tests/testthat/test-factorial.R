4357a2cea14ade4ae5311367ecf6984fa9172ad6
Henning Lorenzen <henning.lorenzen.ext@bayer.com>
2022-08-22 17:56:19 +0200

    tests: add some meaningful cases for factorial()

--- tests/testthat/test-factorial.R
+++ tests/testthat/test-factorial.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Negative arguments are rejected", {
+  expect_error(object = factorial(-1),
+               regexp = "Assertion on 'n' failed: Element 1 is not >= 0.",
+               fixed = TRUE)
+})
+
+
+test_that("Fractional arguments are rejected", {
+  expect_error(object = factorial(0.5),
+               regexp = "Assertion on 'n' failed: Must be of type 'single integerish value', not 'double'.",
+               fixed = TRUE)
+})
+
+
+test_that("Values for 0 and 1 are correct", {
+  expect_equal(factorial(0), 1)
+  expect_equal(factorial(1), 1)
 })
